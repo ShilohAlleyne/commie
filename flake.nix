d461b8a120cf43c07cc337773d90f16df8a8b61a
ShilohAlleyne <ShilohAlleyne@gmail.com>
2025-10-28 22:41:58 +0000

    added package discription

--- flake.nix
+++ flake.nix
@@ -2,53 +2,60 @@
     description = "A very basic Rust dev env flake";
 
     inputs = {
-        nixpkgs-stable.url = "github:NixOS/nixpkgs";
+        nixpkgs-stable.url   = "github:NixOS/nixpkgs";
         nixpkgs-unstable.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
+        rust-overlay.url     = "github:oxalica/rust-overlay";
+        flake-utils.url      = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs-stable, nixpkgs-unstable, flake-utils }:
+    outputs = { self, nixpkgs-stable, nixpkgs-unstable, rust-overlay, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
         let
-            rust-overlay = import (builtins.fetchTarball {
-                url = "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
-                sha256 = "sha256:0vic9rv7p1yr390j0bq9df03glc37zqsx67msazsxz82114q5jgg";
-            });
-
-            pkgs = import nixpkgs-stable {
-                inherit system;
-                overlays = [ rust-overlay ];
+            overlays = [ (import rust-overlay) ];
+            pkgs-stable = import nixpkgs-stable {
+                inherit system overlays;
             };
 
-            rustPackage = pkgs.rustPlatform.buildRustPackage {
-                pname = "commie";
-                version = "0.1.0";
-                src = ./.;
-                cargoLock = {
-                    lockFile = ./Cargo.lock;
+            pkgs-unstable = import nixpkgs-unstable {
+                inherit system overlays;
+            };
+
+            manifest = (pkgs-stable.lib.importTOML ./Cargo.toml).package;
+
+            rustPackage = pkgs-stable.rustPlatform.buildRustPackage {
+                inherit (manifest) name version; 
+
+                meta = with pkgs-stable.lib; {
+                    description = manifest.description;
+                    # Add other optional meta fields:
+                    # homepage = manifest.homepage; # if you have this in Cargo.toml
+                    license = licenses.mit;
                 };
-                meta.mainProgram = "commie";
+
+                cargoLock.lockFile = ./Cargo.lock;
+                src = pkgs-stable.lib.cleanSource ./.;
             };
         in
         {
-            devShells.default = pkgs.mkShell {
+            devShells.default = with pkgs-stable; mkShell {
+                name = "${manifest.name}-devshell";
+
                 buildInputs = [
-                    (pkgs.rust-bin.stable.latest.default.override {
-                        extensions = [ "rust-src" ];
-                    })
-                    pkgs.cargo
-                    pkgs.rustup
+                    pkgs-stable.cargo
+                    pkgs-stable.rustup
+                    pkgs-stable.rust-analyzer
                 ];
+
                 shellHook = ''
-                    rustup component add rust-analyzer
+                    export PATH="${pkgs-stable.rust-analyzer}/bin:$PATH"
+                    echo "Using rust-analyzer from Nixpkgs: $(which rust-analyzer)"
                 '';
             };
 
             packages.default = rustPackage;
 
-            apps.default = {
-                type = "app";
-                program = pkgs.lib.getExe rustPackage;
+            apps.default = flake-utils.lib.mkApp {
+                drv = rustPackage;
             };
         }
     );
